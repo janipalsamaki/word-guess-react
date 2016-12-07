cbabbdfc7d89ae73e0494c3cad9477491999d624
Jani Palsamäki <jani.palsamaki@gmail.com>
2016-12-07 22:07:44 +0200

    Added more English words.

--- src/dictionaries/dictionary-en.js
+++ src/dictionaries/dictionary-en.js
@@ -12,6 +12,26 @@ const dictionary_en = [
     words: ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
   },
   {
+    category: 'Pets',
+    words: ['Dog','Cat','Hamster','Guinea pig','Turtle','Parrot','Budgerigar']
+  },
+  {
+    category: 'African animals',
+    words: ['Lion','Camel','Elephant','Giraffe','Zebra','Rhino','Hippo','Antelope','Hyena','Scorpion','Baboon','Gorilla','Chimpanzee','Constrictor']
+  },
+  {
+    category: 'Drinks',
+    words: ['Lemonade','Strawberry juice','Apple juice','Water','Milk','Buttermilk','Orange juice','Pineapple juice','Mineral water','Beer','Whiskey','Rum','Kvass']
+  },
+  {
+    category: 'Furniture',
+    words: ['Desk','Sofa','Chair','Bed','Bookshelf','Dresser','Armchair']
+  },
+  {
+    category: 'Sports',
+    words: ['Soccer','Basketball','Handball','Volleyball','Ice hockey','Javelin throw','Floorball','Discus throw','Figure skating','Ski jumping','Triathlon']
+  },
+  {
     category: 'Star Wars characters',
     words: ['Jango Fett','Boba Fett','Anakin Skywalker','Luke Skywalker','Han Solo','Chewbacca','Darth Maul','Darth Vader','Jabba the Hutt','Jar Jar Binks','Obi-Wan Kenobi','Yoda','Finn','Kylo Ren','Darth Sidious']
   },
